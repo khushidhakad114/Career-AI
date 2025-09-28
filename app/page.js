fd205333257eb49b0fc4683333834cd3b662ff17
Khushi <khushidhakad2003@gmail.com>
2025-09-28 22:25:19 +0530

    transition

--- app/page.js
+++ app/page.js
@@ -1,3 +1,16 @@
+import HeroSection from "@/components/hero";
+
 export default function Home() {
-  return <div>made by khushi dhakad</div>;
+  return (
+    <div>
+      <div className="grid-background"></div>
+      <HeroSection />
+
+      <section>
+        <div>
+          <h2>Powerful Features for Your Career Growth</h2>
+        </div>
+      </section>
+    </div>
+  );
 }
