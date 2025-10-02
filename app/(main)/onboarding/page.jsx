39dd3131973c451446c1bc732fd431511ac1825c
Khushi <khushidhakad2003@gmail.com>
2025-10-02 18:30:08 +0530

    onboarding-form

--- app/(main)/onboarding/page.jsx
+++ app/(main)/onboarding/page.jsx
@@ -1,8 +1,18 @@
-const OnboardingPage = () => {
+import { getUserOnboardingStatus } from "@/actions/user";
+import { industries } from "@/data/industries";
+import { redirect } from "next/navigation";
+import OnboardingForm from "./_components/onboarding-form";
+
+const OnboardingPage = async () => {
   // Check if user is already onboarded
+  const { isOnboarded } = await getUserOnboardingStatus();
+
+  if (isOnboarded) {
+    redirect("/dashboard");
+  }
   return (
     <main>
-      <OnboardingForm />
+      <OnboardingForm industries={industries} />
     </main>
   );
 };
